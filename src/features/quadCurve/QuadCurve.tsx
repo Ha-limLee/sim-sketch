5bf0655f00ac19861780afdd162b762282e15294
LeeHalim <leehalim09@gmail.com>
2022-12-28 22:54:02 +0900

    fixed issue: curve not seen on drawing

--- src/features/quadCurve/QuadCurve.tsx
+++ src/features/quadCurve/QuadCurve.tsx
@@ -9,29 +9,43 @@ export const QuadCurve = ({ id, points, stroke, strokeWidth }: { id: string, poi
         (points[0] + points[2]) / 2,
         (points[1] + points[3]) / 2,
     ]);
+
     const center = [(points[0] + points[2]) / 2, (points[1] + points[3]) / 2];
 
-    const sceneFunc = (ctx: Context, shape: ShapeType) => {
-        if (points.length !== 4)
-            return;
-        ctx.beginPath();
-        ctx.moveTo(points[0], points[1]);
-        ctx.quadraticCurveTo(anchorPoint[0], anchorPoint[1], points[2], points[3]);
-        ctx.fillStrokeShape(shape);
+    const sceneFunc = {
+        onMove (ctx: Context, shape: ShapeType) {
+            if (points.length !== 4)
+                return;
+            ctx.beginPath();
+            ctx.moveTo(points[0], points[1]);
+            ctx.lineTo(points[2], points[3]);
+            ctx.fillStrokeShape(shape);
+        },
+        onEnd (ctx: Context, shape: ShapeType) {
+            if (points.length !== 4)
+                return;
+            ctx.beginPath();
+            ctx.moveTo(points[0], points[1]);
+            ctx.quadraticCurveTo(anchorPoint[0], anchorPoint[1], points[2], points[3]);
+            ctx.fillStrokeShape(shape);
+        },
     };
 
     const anchorEvent = {
+        handleMouseDown(e: KonvaEventObject<MouseEvent>) {
+            e.cancelBubble = true;
+        },
         handleDragMove (e: KonvaEventObject<DragEvent>) {
             setAnchorPoint([e.evt.offsetX, e.evt.offsetY]);
         },
-        handleMouseDown(e: KonvaEventObject<MouseEvent>) {
-            e.cancelBubble = true;
-        }
+        handleDragEnd (e: KonvaEventObject<DragEvent>) {
+            console.log('end');
+        },
     };
 
     return (
         <>
-            <Shape stroke={stroke} strokeWidth={strokeWidth} sceneFunc={sceneFunc} />
+            <Shape stroke={stroke} strokeWidth={strokeWidth} sceneFunc={anchorPoint.every(x => x) ? sceneFunc.onEnd : sceneFunc.onMove} />
             { anchorPoint.every(x => x) ?
                 <Circle id={id} x={center[0]} y={center[1]} radius={25} stroke="gray" strokeWidth={5} draggable={true} dash={[10, 10]}
                     onMouseDown={anchorEvent.handleMouseDown} onDragMove={anchorEvent.handleDragMove}
