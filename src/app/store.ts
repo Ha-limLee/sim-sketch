f9358c4a8644c52d263f09c6fc1d32c9583355d4
Ha-limLee <leehalim09@gmail.com>
2024-09-06 02:00:56 +0900

    🏷️ fixed type errors

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import toolBoxReducer from '../features/toolBox/toolBoxSlice';
