5f43f84d3938c3e0520039725434958de3a39272
Ha-limLee <leehalim09@gmail.com>
2024-09-10 23:11:51 +0900

    🩹 remove unused id prop

--- packages/client/src/pages/error-page.tsx
+++ packages/client/src/pages/error-page.tsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
   return (
-    <div id="error-page" className={classes["error-page"]}>
+    <div className={classes["error-page"]}>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
